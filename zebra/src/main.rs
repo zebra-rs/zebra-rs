77db19983364dbc6b1cb116296c968b27796964f
Kunihiro Ishiguro <kunihiro@zebra.dev>
2024-04-03 16:06:17 -0700

    Update main.rs to run BGP task.

--- zebra/src/main.rs
+++ zebra/src/main.rs
@@ -1,12 +1,17 @@
 mod config;
 use config::ConfigManager;
+mod bgp;
+use bgp::Bgp;
 use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (config_tx, config_rx) = mpsc::channel(255);
-    config::serve(config_tx).await;
 
+    // vtysh gRPC Server.
+    config::serve(config_tx.clone()).await;
+
+    // Set ${HOME}/.zebra/yang for YANG path.
     let home = dirs::home_dir();
     let path = if let Some(mut home) = home {
         home.push(".zebra");
@@ -16,13 +21,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "./yang/...".to_string()
     };
 
+    // Configuration manager.
     let mut cm = ConfigManager::new(path, config_rx);
+
+    // BGP task.
+    let mut bgp = Bgp::new(config_tx.clone());
+
     println!("zebra: started");
 
     loop {
         tokio::select! {
-            Some(m) = cm.rx.recv() => {
-                cm.process_message(m);
+            Some(msg) = cm.rx.recv() => {
+                cm.process_message(msg);
+            }
+            Some(msg) = bgp.rx.recv() => {
+                bgp.process_message(msg)
             }
         }
     }
