2c5393074e6f0b64df3d35b1e0652c9c0adebd5a
Kunihiro Ishiguro <kunihiro@zebra.dev>
2024-04-09 15:27:53 -0700

    Initial commit for RIB for macos. (#5)

--- zebra/src/rib/mod.rs
+++ zebra/src/rib/mod.rs
@@ -1,4 +1,7 @@
 pub mod handler;
 pub use handler::{serve, Rib};
 
+pub mod link;
+pub use link::LinkFlags;
+
 pub mod os;
--- zebra/src/rib/os/netlink.rs
+++ zebra/src/rib/os/netlink.rs
@@ -1,10 +1,11 @@
-use super::message::{OsLink, OsMessage};
+use super::message::{OsAddress, OsLink, OsMessage, OsRoute};
 use futures::stream::StreamExt;
 use futures::stream::TryStreamExt;
 use netlink_packet_core::{NetlinkMessage, NetlinkPayload};
-use netlink_packet_route::{
-    address::AddressAttribute, link::LinkAttribute, route::RouteAttribute, RouteNetlinkMessage,
-};
+use netlink_packet_route::address::{AddressAttribute, AddressMessage};
+use netlink_packet_route::link::{LinkAttribute, LinkMessage};
+use netlink_packet_route::route::{RouteAttribute, RouteMessage};
+use netlink_packet_route::RouteNetlinkMessage;
 use netlink_sys::{AsyncSocket, SocketAddr};
 use rtnetlink::{
     constants::{
@@ -14,84 +15,118 @@ use rtnetlink::{
 };
 use tokio::sync::mpsc::UnboundedSender;
 
-fn process_msg(msg: NetlinkMessage<RouteNetlinkMessage>, tx: UnboundedSender<OsMessage>) {
-    match msg.payload {
-        NetlinkPayload::InnerMessage(msg) => match msg {
-            RouteNetlinkMessage::NewLink(msg) => {
-                println!("NewLink index: {}", msg.header.index);
-                for attr in msg.attributes.iter() {
-                    match attr {
-                        LinkAttribute::IfName(name) => {
-                            println!("IfName {}", name);
-                        }
-                        LinkAttribute::NewIfIndex(ifindex) => {
-                            println!("IfIndex {}", ifindex);
-                        }
-                        LinkAttribute::Mtu(mtu) => {
-                            println!("MTU {}", mtu);
-                        }
-                        _ => {
-                            //
-                        }
-                    }
-                }
-                let link = OsLink { index: 0u32 };
-                let mes = OsMessage::NewLink(link);
-                tx.send(mes).unwrap();
+fn link_from_msg(msg: LinkMessage) -> OsLink {
+    let mut link = OsLink::new();
+    link.index = msg.header.index;
+    for flag in msg.header.flags.iter() {
+        println!("F: {:?}", flag);
+    }
+    for attr in msg.attributes.into_iter() {
+        match attr {
+            LinkAttribute::IfName(name) => {
+                link.name = name;
             }
-            RouteNetlinkMessage::DelLink(_) => {
-                println!("DelLink");
+            LinkAttribute::Mtu(mtu) => {
+                link.mtu = mtu;
             }
-            RouteNetlinkMessage::NewAddress(_) => {
-                println!("NewAddress");
-            }
-            RouteNetlinkMessage::DelAddress(_) => {
-                println!("DelAddress");
-            }
-            RouteNetlinkMessage::NewRoute(_) => {
-                println!("NewRoute");
-            }
-            RouteNetlinkMessage::DelRoute(_) => {
-                println!("DelRoute");
+            _ => {}
+        }
+    }
+
+    link
+}
+
+fn addr_from_msg(msg: AddressMessage) -> OsAddress {
+    let addr = OsAddress::new();
+    for attr in msg.attributes.into_iter() {
+        match attr {
+            AddressAttribute::Address(_) => {
+                //
             }
             _ => {
                 //
             }
+        }
+    }
+    addr
+}
+
+fn route_from_msg(msg: RouteMessage) -> OsRoute {
+    let route = OsRoute::new();
+    for attr in msg.attributes.into_iter() {
+        match attr {
+            RouteAttribute::Destination(_) => {
+                //
+            }
+            RouteAttribute::Gateway(_) => {
+                //
+            }
+            _ => {
+                //
+            }
+        }
+    }
+    route
+}
+
+fn process_msg(msg: NetlinkMessage<RouteNetlinkMessage>, tx: UnboundedSender<OsMessage>) {
+    match msg.payload {
+        NetlinkPayload::InnerMessage(msg) => match msg {
+            RouteNetlinkMessage::NewLink(msg) => {
+                let link = link_from_msg(msg);
+                let msg = OsMessage::NewLink(link);
+                tx.send(msg).unwrap();
+            }
+            RouteNetlinkMessage::DelLink(msg) => {
+                let link = link_from_msg(msg);
+                let msg = OsMessage::DelLink(link);
+                tx.send(msg).unwrap();
+            }
+            RouteNetlinkMessage::NewAddress(msg) => {
+                let addr = addr_from_msg(msg);
+                let msg = OsMessage::NewAddress(addr);
+                tx.send(msg).unwrap();
+            }
+            RouteNetlinkMessage::DelAddress(msg) => {
+                let addr = addr_from_msg(msg);
+                let msg = OsMessage::DelAddress(addr);
+                tx.send(msg).unwrap();
+            }
+            RouteNetlinkMessage::NewRoute(msg) => {
+                let route = route_from_msg(msg);
+                let msg = OsMessage::NewRoute(route);
+                tx.send(msg).unwrap();
+            }
+            RouteNetlinkMessage::DelRoute(msg) => {
+                let route = route_from_msg(msg);
+                let msg = OsMessage::DelRoute(route);
+                tx.send(msg).unwrap();
+            }
+            _ => {
+                // Not expecting.
+            }
         },
-        _ => {}
+        _ => {
+            // We only interested in updates.
+        }
     }
 }
 
 async fn link_dump(handle: rtnetlink::Handle, tx: UnboundedSender<OsMessage>) {
     let mut links = handle.link().get().execute();
     while let Some(msg) = links.try_next().await.unwrap() {
-        for nla in msg.attributes.into_iter() {
-            if let LinkAttribute::IfName(name) = nla {
-                println!("found link {} ({})", msg.header.index, name);
-            }
-        }
-        // Get address.w
+        let link = link_from_msg(msg);
+        let msg = OsMessage::NewLink(link);
+        tx.send(msg).unwrap();
     }
 }
 
 async fn address_dump(handle: rtnetlink::Handle, tx: UnboundedSender<OsMessage>) {
     let mut addresses = handle.address().get().execute();
     while let Some(msg) = addresses.try_next().await.unwrap() {
-        for attr in msg.attributes.into_iter() {
-            match attr {
-                AddressAttribute::Address(addr) => {
-                    println!("{:?}", addr);
-                }
-                _ => {
-                    //
-                }
-            }
-
-            // if let LinkAttribute::IfName(name) = nla {
-            //     println!("found link {} ({})", msg.header.index, name);
-            // }
-        }
-        // Get address.
+        let addr = addr_from_msg(msg);
+        let msg = OsMessage::NewAddress(addr);
+        tx.send(msg).unwrap();
     }
 }
 
@@ -101,17 +136,10 @@ async fn route_dump(
     ip_version: IpVersion,
 ) {
     let mut routes = handle.route().get(ip_version).execute();
-    while let Some(route) = routes.try_next().await.unwrap() {
-        for attr in route.attributes.iter() {
-            match attr {
-                RouteAttribute::Destination(_) => {
-                    //
-                }
-                _ => {
-                    //
-                }
-            }
-        }
+    while let Some(msg) = routes.try_next().await.unwrap() {
+        let route = route_from_msg(msg);
+        let msg = OsMessage::NewRoute(route);
+        tx.send(msg).unwrap();
     }
 }
 
