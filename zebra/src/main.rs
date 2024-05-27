0ae7608899f4ceeafeadffbb464f7a0be161fd6c
Kunihiro Ishiguro <kunihiro@zebra.dev>
2024-05-26 21:50:28 -0700

    -yang-path option (#57)
    * leaflist delete entry.
    
    * Start policy implementation.
    
    * Add --yang-path option to zebra.

--- zebra/src/main.rs
+++ zebra/src/main.rs
@@ -8,32 +8,48 @@ use bgp::Bgp;
 mod rib;
 use rib::Rib;
 mod policy;
+use clap::Parser;
 
-fn system_path() -> PathBuf {
-    let mut home = dirs::home_dir().unwrap();
-    home.push(".zebra");
-    if home.is_dir() {
-        home
-    } else {
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Arg {
+    #[arg(short, long, help = "YANG load path", default_value = "")]
+    yang_path: String,
+}
+
+fn system_path(arg: &Arg) -> PathBuf {
+    if !arg.yang_path.is_empty() {
         let mut path = PathBuf::new();
-        path.push("etc");
-        path.push("zebra");
-        if path.is_dir() {
-            path
+        path.push(&arg.yang_path);
+        path
+    } else {
+        let mut home = dirs::home_dir().unwrap();
+        home.push(".zebra");
+        if home.is_dir() {
+            home
         } else {
-            std::env::current_dir().unwrap()
+            let mut path = PathBuf::new();
+            path.push("etc");
+            path.push("zebra");
+            if path.is_dir() {
+                path
+            } else {
+                std::env::current_dir().unwrap()
+            }
         }
     }
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let arg = Arg::parse();
+
     let mut rib = Rib::new()?;
 
     let bgp = Bgp::new(rib.api.tx.clone());
     rib.subscribe(bgp.redist.tx.clone());
 
-    let mut config = ConfigManager::new(system_path())?;
+    let mut config = ConfigManager::new(system_path(&arg))?;
     config.subscribe("rib", rib.cm.tx.clone());
     config.subscribe("bgp", bgp.cm.tx.clone());
 
