b58dbbc5fd95eb2c00373484ef295593f2d40f62
Kunihiro Ishiguro <kunihiro@zebra.dev>
2024-09-05 23:46:50 -0700

    New command vtyctl.

---
+++ vtyctl/src/main.rs
@@ -0,0 +1,40 @@
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+pub mod vtysh {
+    tonic::include_proto!("vtysh");
+}
+pub mod apply;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(disable_help_flag = true)]
+    Apply {
+        #[arg(short, long, default_value = "127.0.0.1")]
+        host: String,
+
+        #[arg(short, long, help = "Base URL of the server", default_value = "")]
+        filename: String,
+    },
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Apply { host, filename }) => {
+            apply::apply(host, filename).await?;
+        }
+        None => {}
+    }
+
+    Ok(())
+}
