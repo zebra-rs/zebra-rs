68da6b2a8c1c77cb0798f7bf4e7c9c51427f7b84
Kunihiro Ishiguro <kunihiro@zebra.dev>
2024-12-16 08:33:51 -0800

    Call NFSM.

--- zebra/src/ospf/mod.rs
+++ zebra/src/ospf/mod.rs
@@ -2,15 +2,19 @@ pub mod inst;
 pub use inst::{Message, Ospf, ShowCallback};
 
 pub mod link;
-pub use link::OspfLink;
+pub use link::{OspfIdentity, OspfLink};
+
+pub mod neigh;
+pub use neigh::OspfNeighbor;
+
+pub mod task;
+pub use task::{Timer, TimerType};
 
 pub mod addr;
 
 pub mod area;
 
 pub mod show;
-
-pub mod neigh;
 
 pub mod config;
 
@@ -23,5 +27,3 @@ pub mod ifsm;
 pub mod nfsm;
 
 pub mod packet;
-
-pub mod task;
