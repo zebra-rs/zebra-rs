d02c679596f39b0f38b525db38a493fb861cd1e9
Kunihiro Ishiguro <kunihiro@zebra.dev>
2025-06-21 08:46:15 -0700

    Change zebra-rs binary path.

--- packaging/scripts/postinstall.sh
+++ packaging/scripts/postinstall.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-setcap 'cap_net_bind_service=ep cap_net_admin=ep cap_net_bind_service=ep cap_net_broadcast=ep cap_net_raw=ep' /usr/sbin/zebra-rs
+setcap 'cap_net_bind_service=ep cap_net_admin=ep cap_net_bind_service=ep cap_net_broadcast=ep cap_net_raw=ep' /usr/bin/zebra-rs
 sudo systemctl restart systemd-modules-load.service
