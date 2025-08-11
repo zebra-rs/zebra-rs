87465e0cd5cbf6d9a70f1434443c3c7212ae4040
Kunihiro Ishiguro <kunihiro@zebra.dev>
2025-08-10 20:27:34 -0700

    Refactor timer related code to timer.rs.

--- zebra-rs/src/context/task.rs
+++ zebra-rs/src/context/task.rs
@@ -1,6 +1,6 @@
-#![allow(dead_code)]
 use std::future::Future;
-use std::time::Duration;
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
 use tokio::sync::mpsc::{self, UnboundedSender};
 use tokio::task;
 
@@ -37,7 +37,9 @@ impl<T> Drop for Task<T> {
 
 #[derive(Debug)]
 pub struct Timer {
-    pub tx: UnboundedSender<TimerMessage>,
+    tx: UnboundedSender<TimerMessage>,
+    duration: Duration,              // Store the timer duration
+    last_reset: Arc<Mutex<Instant>>, // Track the last reset time
 }
 
 #[derive(Debug)]
@@ -52,19 +54,28 @@ pub enum TimerType {
 }
 
 impl Timer {
-    pub fn new<F, Fut>(duration: Duration, typ: TimerType, mut cb: F) -> Timer
+    pub fn new<F, Fut>(sec: u64, typ: TimerType, mut cb: F) -> Timer
     where
         F: FnMut() -> Fut + Send + 'static,
         Fut: Future<Output = ()> + Send,
     {
-        let (tx, mut rx) = mpsc::unbounded_channel();
+        // Make it sure sec is not zero.
+        let sec = if sec == 0 { 1 } else { sec };
 
+        // println!("Timer create: duration {}", sec);
+        let duration = Duration::new(sec, 0);
+
+        let (tx, mut rx) = mpsc::unbounded_channel();
+        let last_reset = Arc::new(Mutex::new(Instant::now()));
+
+        let last_reset_clone = last_reset.clone();
         tokio::spawn(async move {
             let mut interval = tokio::time::interval(duration);
             _ = interval.tick().await;
             loop {
                 tokio::select! {
                     _ = interval.tick() => {
+                        // println!("Timer expired {}", sec);
                         (cb)().await;
                         if typ == TimerType::Once {
                             break;
@@ -73,6 +84,7 @@ impl Timer {
                     message = rx.recv() => {
                         match message {
                             Some(TimerMessage::Refresh)=> {
+                                *last_reset_clone.lock().unwrap() = Instant::now();
                                 interval = tokio::time::interval(duration);
                                 _ = interval.tick().await;
                             }
@@ -82,14 +94,46 @@ impl Timer {
                 }
             }
         });
-        Timer { tx }
+
+        Timer {
+            tx,
+            duration,
+            last_reset,
+        }
+    }
+
+    pub fn once<F, Fut>(sec: u64, cb: F) -> Timer
+    where
+        F: FnMut() -> Fut + Send + 'static,
+        Fut: Future<Output = ()> + Send,
+    {
+        Self::new(sec, TimerType::Once, cb)
+    }
+
+    pub fn repeat<F, Fut>(sec: u64, cb: F) -> Timer
+    where
+        F: FnMut() -> Fut + Send + 'static,
+        Fut: Future<Output = ()> + Send,
+    {
+        Self::new(sec, TimerType::Infinite, cb)
+    }
+
+    /// Refresh the timer (resets the timer countdown)
+    pub fn refresh(&self) {
+        let _ = self.tx.send(TimerMessage::Refresh);
     }
 
     pub fn second(sec: u64) -> Duration {
         Duration::new(sec, 0)
     }
 
-    pub fn refresh(&self) {
-        let _ = self.tx.send(TimerMessage::Refresh);
+    /// Get the remaining seconds until the next tick
+    pub fn rem_sec(&self) -> u64 {
+        let elapsed = self.last_reset.lock().unwrap().elapsed();
+        if elapsed >= self.duration {
+            0
+        } else {
+            (self.duration - elapsed).as_secs()
+        }
     }
 }
