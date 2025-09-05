ba9da4811bd541064516c027038eb03259cffcd0
Kunihiro Ishiguro <kunihiro@zebra.dev>
2025-09-04 19:50:44 -0700

    OSPF network area configuration.

--- zebra-rs/src/ospf/area.rs
+++ zebra-rs/src/ospf/area.rs
@@ -1,66 +1,48 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
-pub struct OspfAreaMap {
-    pub mapping: BTreeMap<u32, usize>,
-    pub vec: Vec<Option<OspfArea>>,
-}
-
-pub trait MapVec {
-    fn id(&self) -> usize;
-    fn set_id(&mut self, id: usize);
-}
+pub struct OspfAreaMap(BTreeMap<u32, OspfArea>);
 
 impl OspfAreaMap {
     pub fn new() -> Self {
-        Self {
-            mapping: BTreeMap::new(),
-            vec: Vec::new(),
-        }
+        Self(BTreeMap::new())
     }
 
-    pub fn get(&self, area_id: u32) -> Option<&OspfArea> {
-        let index = *self.mapping.get(&area_id)?;
-        self.vec.get(index)?.as_ref()
+    pub fn get(&self, id: u32) -> Option<&OspfArea> {
+        self.0.get(&id)
     }
 
-    pub fn get_mut(&mut self, area_id: u32) -> Option<&mut OspfArea> {
-        let index = *self.mapping.get(&area_id)?;
-        self.vec.get_mut(index)?.as_mut()
+    pub fn get_mut(&mut self, id: u32) -> Option<&mut OspfArea> {
+        self.0.get_mut(&id)
     }
 
-    pub fn fetch(&mut self, area_id: u32) -> Option<&mut OspfArea> {
-        let index = if let Some(&index) = self.mapping.get(&area_id) {
-            index
-        } else {
-            let index = self.vec.len();
-            self.mapping.insert(area_id, index);
-            self.vec.push(None);
-            index
-        };
-        if self.vec[index].is_none() {
-            self.vec[index] = Some(OspfArea::new(index, area_id));
-        }
-        self.vec.get_mut(index).and_then(|slot| slot.as_mut())
+    pub fn insert(&mut self, area_id: u32, area: OspfArea) {
+        self.0.insert(area_id, area);
+    }
+
+    pub fn remove(&mut self, area_id: u32) -> Option<OspfArea> {
+        self.0.remove(&area_id)
+    }
+
+    pub fn fetch(&mut self, area_id: u32) -> &mut OspfArea {
+        self.0
+            .entry(area_id)
+            .or_insert_with(|| OspfArea::new(area_id))
     }
 }
 
 pub struct OspfArea {
-    pub id: usize,
-    pub area_id: u32,
+    // OSPF area id.  This value may be treated as IPv4 address.
+    pub id: u32,
+
+    // Set of interfaces belongs to this area.
+    pub links: BTreeSet<usize>,
 }
 
 impl OspfArea {
-    pub fn new(id: usize, area_id: u32) -> Self {
-        Self { id, area_id }
-    }
-}
-
-impl MapVec for OspfArea {
-    fn id(&self) -> usize {
-        self.id
-    }
-
-    fn set_id(&mut self, id: usize) {
-        self.id = id;
+    pub fn new(id: u32) -> Self {
+        Self {
+            id,
+            links: BTreeSet::new(),
+        }
     }
 }
