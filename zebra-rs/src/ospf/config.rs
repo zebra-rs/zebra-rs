2f74eba9507e90201d1cbda4fc7510fd92d77361
Kunihiro Ishiguro <kunihiro@zebra.dev>
2025-09-04 19:50:44 -0700

    OSPF network area configuration.

--- zebra-rs/src/ospf/config.rs
+++ zebra-rs/src/ospf/config.rs
@@ -1,11 +1,17 @@
+use std::collections::BTreeMap;
+
+use ipnet::Ipv4Net;
+use prefix_trie::PrefixMap;
+
+use super::OspfLink;
 use super::{Ospf, addr::OspfAddr, area::OspfArea};
 
 use crate::config::{Args, ConfigOp};
-use crate::ospf::area::MapVec;
+use crate::rib::util::*;
 
 #[derive(Default)]
 pub struct OspfNetworkConfig {
-    pub area: usize,
+    pub area_id: u32,
     pub addr: Option<OspfAddr>,
 }
 
@@ -15,29 +21,69 @@ impl Ospf {
     }
 }
 
-// network 192.168.10.0/24 area 0
-// network 192.168.3.0/24 area 0
+fn config_ospf_network_apply(
+    links: &mut BTreeMap<u32, OspfLink>,
+    table: &PrefixMap<Ipv4Net, OspfNetworkConfig>,
+) {
+    for (_, link) in links.iter() {
+        // Enabled -> Disabled
+        // Disabled -> Enabled
+        // Enabled -> Enabled (area changed)
+        let curr = link.enabled;
+        let curr_id = link.area_id;
+
+        let mut next = false;
+        let mut next_id = 0;
+
+        for addr in link.addr.iter() {
+            let prefix = addr.prefix.addr().to_host_prefix();
+            if let Some((_, network_config)) = table.get_lpm(&prefix) {
+                // Found network configuration, break at here.
+                next = true;
+                next_id = network_config.area_id;
+                break;
+            }
+        }
+
+        if curr {
+            if next {
+                if curr_id != next_id {
+                    // Enalbed -> Enabled
+                    // Area id has been changed.
+                    println!("LINK: ");
+                    // Remove from old area.
+                    // Add to old area.
+                }
+            } else {
+                // Enabled -> Disabled.
+                // Stop event to the link.
+                println!("LINK: ");
+                // Delete from old area.
+            }
+        } else {
+            if next {
+                // Disabled -> Enabled.
+                // Start event to the link.
+                println!("LINK: ");
+                // Add to new area.
+            }
+        }
+    }
+}
 
 fn config_ospf_network(ospf: &mut Ospf, mut args: Args, op: ConfigOp) -> Option<()> {
     let network = args.v4net()?;
     let area_id = args.u32()?;
 
     if op.is_set() {
-        let area = ospf.areas.fetch(area_id)?;
-        let network_area = ospf.table.entry(network).or_default();
-        network_area.area = area.id();
+        let area = ospf.areas.fetch(area_id);
+        let network_config = ospf.table.entry(network).or_default();
+        network_config.area_id = area.id;
     } else {
         ospf.table.remove(&network);
     }
 
-    for (_, link) in ospf.links.iter() {
-        let enabled = link.enabled;
-        let area_id = link.area_id;
-
-        for addr in link.addr.iter() {
-            // ospf.table.get();
-        }
-    }
+    config_ospf_network_apply(&mut ospf.links, &ospf.table);
 
     Some(())
 }
