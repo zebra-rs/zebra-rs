dc71235bdf92a6e263f724be60131d04acbd8038
Kunihiro Ishiguro <kunihiro@zebra.dev>
2025-09-07 07:56:12 -0700

    Change area key from u32 to Ipv4Addr.

--- zebra-rs/src/ospf/area.rs
+++ zebra-rs/src/ospf/area.rs
@@ -1,29 +1,32 @@
 use std::collections::{BTreeMap, BTreeSet};
+use std::net::Ipv4Addr;
 
-pub struct OspfAreaMap(BTreeMap<u32, OspfArea>);
+pub struct OspfAreaMap(BTreeMap<Ipv4Addr, OspfArea>);
 
 impl OspfAreaMap {
     pub fn new() -> Self {
-        Self(BTreeMap::new())
+        let mut areas = Self(BTreeMap::new());
+        areas.fetch(Ipv4Addr::UNSPECIFIED);
+        areas
     }
 
-    pub fn get(&self, id: u32) -> Option<&OspfArea> {
+    pub fn get(&self, id: Ipv4Addr) -> Option<&OspfArea> {
         self.0.get(&id)
     }
 
-    pub fn get_mut(&mut self, id: u32) -> Option<&mut OspfArea> {
+    pub fn get_mut(&mut self, id: Ipv4Addr) -> Option<&mut OspfArea> {
         self.0.get_mut(&id)
     }
 
-    pub fn insert(&mut self, area_id: u32, area: OspfArea) {
+    pub fn insert(&mut self, area_id: Ipv4Addr, area: OspfArea) {
         self.0.insert(area_id, area);
     }
 
-    pub fn remove(&mut self, area_id: u32) -> Option<OspfArea> {
+    pub fn remove(&mut self, area_id: Ipv4Addr) -> Option<OspfArea> {
         self.0.remove(&area_id)
     }
 
-    pub fn fetch(&mut self, area_id: u32) -> &mut OspfArea {
+    pub fn fetch(&mut self, area_id: Ipv4Addr) -> &mut OspfArea {
         self.0
             .entry(area_id)
             .or_insert_with(|| OspfArea::new(area_id))
@@ -32,14 +35,14 @@ impl OspfAreaMap {
 
 pub struct OspfArea {
     // OSPF area id.  This value may be treated as IPv4 address.
-    pub id: u32,
+    pub id: Ipv4Addr,
 
     // Set of interfaces belongs to this area.
     pub links: BTreeSet<u32>,
 }
 
 impl OspfArea {
-    pub fn new(id: u32) -> Self {
+    pub fn new(id: Ipv4Addr) -> Self {
         Self {
             id,
             links: BTreeSet::new(),
