3450e5f8a09502d26a9e0ca507c156b007d0dcbb
Kunihiro Ishiguro <kunihiro@zebra.dev>
2025-09-07 18:11:52 -0700

    LSDB lookup.

--- zebra-rs/src/ospf/lsdb.rs
+++ zebra-rs/src/ospf/lsdb.rs
@@ -1,19 +1,65 @@
 use std::{collections::BTreeMap, net::Ipv4Addr};
 
-use ospf_packet::OspfLsa;
+use ospf_packet::{OspfLsType, OspfLsa};
+
+pub type LsTable = BTreeMap<(u32, Ipv4Addr), OspfLsa>;
 
 pub struct Lsdb {
-    pub db: BTreeMap<(u32, Ipv4Addr), OspfLsa>,
+    pub tables: LsTypes<LsTable>,
+}
+
+#[derive(Default, Debug)]
+pub struct LsTypes<T> {
+    pub router: T,
+    pub network: T,
+    pub summary: T,
+    pub summary_asbr: T,
+    pub as_external: T,
+    pub unknown: T,
+}
+
+impl<T> LsTypes<T> {
+    pub fn get(&self, ls_type: &OspfLsType) -> &T {
+        match ls_type {
+            OspfLsType::Router => &self.router,
+            OspfLsType::Network => &self.network,
+            OspfLsType::Summary => &self.summary,
+            OspfLsType::SummaryAsbr => &self.summary_asbr,
+            OspfLsType::AsExternal => &self.as_external,
+            _ => &self.unknown,
+        }
+    }
+
+    pub fn get_mut(&mut self, ls_type: &OspfLsType) -> &mut T {
+        match ls_type {
+            OspfLsType::Router => &mut self.router,
+            OspfLsType::Network => &mut self.network,
+            OspfLsType::Summary => &mut self.summary,
+            OspfLsType::SummaryAsbr => &mut self.summary_asbr,
+            OspfLsType::AsExternal => &mut self.as_external,
+            _ => &mut self.unknown,
+        }
+    }
 }
 
 impl Lsdb {
     pub fn new() -> Self {
         Self {
-            db: BTreeMap::new(),
+            tables: LsTypes::<LsTable>::default(),
         }
     }
 
     pub fn is_empty(&self) -> bool {
-        self.db.is_empty()
+        // self.db.is_empty()
+        false
+    }
+
+    pub fn lookup_by_id(
+        &self,
+        ls_type: OspfLsType,
+        ls_id: u32,
+        adv_router: &Ipv4Addr,
+    ) -> Option<OspfLsa> {
+        self.tables.get(&ls_type).get(&ls_id)
     }
 }
--- zebra-rs/src/ospf/packet.rs
+++ zebra-rs/src/ospf/packet.rs
@@ -1,7 +1,7 @@
 use std::net::Ipv4Addr;
 
 use ipnet::Ipv4Net;
-use ospf_packet::{OspfDbDesc, OspfHello, OspfLsType, Ospfv2Packet, Ospfv2Payload};
+use ospf_packet::{OspfDbDesc, OspfHello, OspfLsType, OspfLsa, Ospfv2Packet, Ospfv2Payload};
 
 use crate::ospf::nfsm::{ospf_db_summary_isempty, ospf_nfsm};
 
@@ -210,23 +210,28 @@ fn lsa_flood_scope(ls_type: OspfLsType) -> FloodScope {
     }
 }
 
-fn ospf_lsa_lookup(oi: &mut OspfInterface, ls_type: OspfLsType, ls_id: u32, adv_router: &Ipv4Addr) {
+fn ospf_lsa_lookup(
+    oi: &mut OspfInterface,
+    ls_type: OspfLsType,
+    ls_id: u32,
+    adv_router: &Ipv4Addr,
+) -> Option<OspfLsa> {
     match lsa_flood_scope(ls_type) {
         FloodScope::Area => {
             println!("FloodScope::Area");
-            oi.lsdb_area;
+            return oi.lsdb_area.lookup_by_id(ls_type, ls_id, adv_router);
         }
         FloodScope::As => {
             println!("FloodScope::As");
-            //
+            None
         }
         FloodScope::Link => {
             println!("FloodScope::Link");
-            //
+            None
         }
         FloodScope::Unknown => {
             println!("FloodScope::Unknown");
-            // Nothing to do.
+            None
         }
     }
 }
