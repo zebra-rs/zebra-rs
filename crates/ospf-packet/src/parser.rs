108fc0e192e56230508a3ea0c408dda0f1ac6d2d
Kunihiro Ishiguro <kunihiro@zebra.dev>
2026-03-05 20:45:09 -0800

    Cosmetic change.

--- crates/ospf-packet/src/parser.rs
+++ crates/ospf-packet/src/parser.rs
@@ -5,14 +5,14 @@ use byteorder::{BigEndian, ByteOrder};
 use bytes::{BufMut, BytesMut};
 use internet_checksum::Checksum;
 use ipnet::Ipv4Net;
-use nom::error::{ErrorKind, make_error};
-use nom::number::complete::{be_u8, be_u24, be_u64};
+use nom::error::{make_error, ErrorKind};
+use nom::number::complete::{be_u24, be_u64, be_u8};
 use nom::{Err, IResult, Needed};
 use nom_derive::*;
 use sr_packet::Algo;
 
 use super::util::{Emit, ParseBe};
-use super::{OspfLsType, OspfType, many0_complete};
+use super::{many0_complete, OspfLsType, OspfType};
 
 // OSPF version.
 const OSPF_VERSION: u8 = 2;
